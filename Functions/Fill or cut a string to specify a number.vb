37558f6d8c4dcadbe18dff31e1ab8ca8357166ec
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2016-02-16 11:45:41 +0100

    Create Fill or cut a string to specify a number.vb

---
+++ Functions/Fill or cut a string to specify a number.vb
@@ -0,0 +1,13 @@
+'Fill or cut a string to specify a number
+Function CompletingString(ByRef StrIn As String, ByRef NbCarMax As Long) As String
+    Dim NbCar As Long: NbCar = Len(StrIn)
+    CompletingString = StrIn
+    If NbCar <= NbCarMax Then
+        Dim i As Long
+        For i = NbCar To NbCarMax - 1 Step 1
+            CompletingString = CompletingString & " "
+        Next i
+    Else
+        CompletingString = Left(CompletingString, NbCarMax)
+    End If
+End Function
