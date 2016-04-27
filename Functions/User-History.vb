6f743e379a546de306483e8234724c3836cfca8e
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2016-04-27 15:53:50 +0200

    Update User-History.vb

--- Functions/User-History.vb
+++ Functions/User-History.vb
@@ -15,13 +15,3 @@ Sub Workbook_SheetChange(ByVal Sh As Object, ByVal Target As Range)
   End With
   MoreFasterCode (False)
 End Sub
-
-Function MoreFasterCode(ByRef Top As Boolean)
-    With Application
-        If Top Then
-            .DisplayAlerts = False: .ScreenUpdating = False: .EnableEvents = False: .Calculation = xlManual
-        Else
-            .Calculation = xlAutomatic: .EnableEvents = True: .ScreenUpdating = True: .DisplayAlerts = True
-        End If
-    End With
-End Function
