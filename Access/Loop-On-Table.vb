2ff6b58bb2d23af15f5a2665105e749844983bdc
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2020-01-17 17:27:35 +0100

    Update Loop-On-Table.vb

--- Access/Loop-On-Table.vb
+++ Access/Loop-On-Table.vb
@@ -2,22 +2,21 @@
 'Loop on access table / boucle sur chaque enregistrement
 '=================================================================================================================
 Sub Loop_on_access_table()
-    'Varable objet "Record Set"
+    'Varable objet "RecordSet"
     Set Table = CurrentDb.TableDefs("TableName").OpenRecordset()
+    '===========================================================================================
     While Not (Table.EOF) 'Tant qu'il reste des enregistrements à traiter
-    '------------------------------------------------------------------------------------------
-        'Par defaut l'objet recordSet est positionné sur le premier enregistrement
+    '===========================================================================================
+        'Par defaut l'objet RecordSet est positionné sur le premier enregistrement
+        Table.Edit '=> edition de l'enregistrement
+        Trash = Table!DataName.Value 'Lecture d'une donnèe
+        Table.Update '=> MàJ enregsitrement modifié
 
-            Table.Edit '=> edition de l'enregistrement
-            
-            Trash = Table!DataName.Value 'Lecture d'une donnèe
-            
-            Table.Update '=> MàJ enregsitrement modifié
-
-        Table.MoveNext '=> enregitrement suivant
-    '------------------------------------------------------------------------------------------
+        'enregitrement suivant
+        Table.MoveNext 
+    '===========================================================================================
     Wend
-    '------------------------------------------------------------------------------------------
+    '===========================================================================================
     'RàZ objets
     TableName.Close: Set TableName = Nothing
 End Sub
